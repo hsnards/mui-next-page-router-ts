c6690d73b27a55690ebed5af80b650cdcc8e9ea6
hassan ardestani <1214hassan1214@gmail.com>
2023-09-27 11:16:14 +0330

    responsive header

---
+++ src/shared/components/layout/Header.tsx
@@ -0,0 +1,41 @@
+import { Button, Container, ListItemButton } from '@mui/material';
+import Image from 'next/image';
+import LogoSrc from '@/assets/images/Logo.png';
+import Menu from '@/assets/icons/Menu.svg';
+import classes from './Header.module.scss';
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <Image className={classes.logo} src={LogoSrc} alt="propision" />
+
+      <ul className={classes.list}>
+        <li>
+          <ListItemButton color="white">سوالات متداول</ListItemButton>
+        </li>
+        <li>
+          <ListItemButton>گزارشات و نمودار‌ها</ListItemButton>
+        </li>
+        <li>
+          <ListItemButton>اشتراک ها</ListItemButton>
+        </li>
+        <li>
+          <ListItemButton>بلاگ</ListItemButton>
+        </li>
+        <li>
+          <ListItemButton>درباره ‌ما</ListItemButton>
+        </li>
+      </ul>
+      <div className={classes['auth-container']}>
+        <Button variant="text">عضویت</Button>
+        <Button variant="contained" color="secondary">
+          ورود
+        </Button>
+      </div>
+
+      <Image className={classes.menu} src={Menu} alt={'menu icon'} />
+
+    </header>
+  );
+};
+
+export default Header;
--- src/shared/components/layout/MainLayout.tsx
+++ src/shared/components/layout/MainLayout.tsx
@@ -1,44 +1,16 @@
 import { Box, Button, Container, List, ListItem, ListItemButton } from '@mui/material';
 import { ComponentPropsWithoutRef } from 'react';
-import LogoSrc from '@/assets/images/Logo.png';
-import Image from 'next/image';
-import classes from './Header.module.scss';
+import back from '@/assets/images/background-small.svg'
+import Header from './Header';
 const MainLayout = ({ children }: ComponentPropsWithoutRef<'div'>) => {
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <Box component={'header'} bgcolor={'#0B1226'} borderBottom={'1.5px solid #121B32'}>
-        <Container
-          maxWidth={'xl'}
-          sx={{ py: '35px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
-        >
-          <Image src={LogoSrc} alt="propision" />
-
-          <ul className={classes.list}>
-            <li>
-              <ListItemButton color="white">سوالات متداول</ListItemButton>
-            </li>
-            <li>
-              <ListItemButton>گزارشات و نمودار‌ها</ListItemButton>
-            </li>
-            <li>
-              <ListItemButton>اشتراک ها</ListItemButton>
-            </li>
-            <li>
-              <ListItemButton>بلاگ</ListItemButton>
-            </li>
-            <li>
-              <ListItemButton>درباره ‌ما</ListItemButton>
-            </li>
-          </ul>
-          <div className={classes['auth-container']}>
-            <Button variant='text'>عضویت</Button>
-            <Button variant="contained" color="secondary">
-              ورود
-            </Button>
-          </div>
-        </Container>
-      </Box>
-      <Box component={'main'} bgcolor={'#0B1226'} flexGrow={1}>
+      <Header />
+      <Box
+        component={'main'}
+        flexGrow={1}
+        sx={{ backgroundImage: `url('${back}')` }}
+      >
         {children}
       </Box>
       <Box component={'footer'} bgcolor={'#0B1226'} borderTop={'1.5px solid #121B32'} p={5}>
